977e66e11d9506be362d5719c30e4d371a12f657
German Cruz <36717136+GermanCorp@users.noreply.github.com>
2018-12-14 14:00:12 -0600

    Reload

--- app/src/main/java/com/smartec/tiendavehiculos/ServerConfig.java
+++ app/src/main/java/com/smartec/tiendavehiculos/ServerConfig.java
@@ -2,7 +2,7 @@ package com.smartec.tiendavehiculos;
 
 public final class ServerConfig {
     public static final String PUERTO ="9001";
-    public static final String SERVIDOR ="http://192.168.43.74:";
+    public static final String SERVIDOR ="http://192.168.0.24:";
     public static final String URL ="/appVehiculos/";
     public static final String URL_BASE = SERVIDOR+PUERTO+URL;
 }
